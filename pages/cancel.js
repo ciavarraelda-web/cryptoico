50ca3bce4490e837f4df4c5bc3101d24dddc3365
ciavarraelda-web <ciavarraelda@gmail.com>
2025-08-31 16:20:46 +0200

    Create cancel.js

---
+++ pages/cancel.js
@@ -0,0 +1,9 @@
+export default function Cancel() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-red-50">
+      <h1 className="text-2xl font-bold text-red-700">
+        ❌ Payment cancelled. Please try again.
+      </h1>
+    </div>
+  )
+}
